ba1da1eda0de19f275ba317f071f5e575ef0bc4d
onjiro <onjiro.mohyahya@gmail.com>
2015-04-16 20:37:27 +0900

    mix phoenix.new

---
+++ config/prod.secret.exs
@@ -0,0 +1,14 @@
+use Mix.Config
+
+# In this file, we keep production configuration that
+# you likely want to automate and keep it away from
+# your version control system.
+config :hello_phoenix, HelloPhoenix.Endpoint,
+  secret_key_base: "G450jjfwgRRAwYKAMdpe2ahaubxExoqBxBrkLlVkR6cqnYYYBeksngRRDLwd8+cX"
+
+# Configure your database
+config :hello_phoenix, HelloPhoenix.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: "postgres",
+  password: "postgres",
+  database: "hello_phoenix_prod"
